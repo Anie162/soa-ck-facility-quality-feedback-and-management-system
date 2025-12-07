e49916d211297e2b735d4a40ab7ea69e6df02436
Anie162 <nhii264705@gmail.com>
2025-12-07 20:44:23 +0700

    ReportingService v7

--- reporting-service/main.py
+++ reporting-service/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from route.reporting_service_route import router as report_router
 
 app = FastAPI(
     title="Reporting Service",
     swagger_ui_parameters={"defaultModelsExpandDepth": -1}
 )
+
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
 
 app.include_router(report_router, prefix="/api/report")
