e94efee1e35cabe3c36ee01c28c48b99e0646b17
Anie162 <nhii264705@gmail.com>
2025-12-07 19:16:48 +0700

    Update Reporting Service v2

--- reporting-service/main.py
+++ reporting-service/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from route.reporting_service_route import route as report_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Reporting Service",
+    swagger_ui_parameters={"defaultModelsExpandDepth": -1}
+)
 
-app.include_router(report_router, prefix="/api")
+app.include_router(report_router, prefix="/api/report")
