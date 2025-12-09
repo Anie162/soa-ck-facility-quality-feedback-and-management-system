8e9a17da4af3b4f7360afed9a55f893aa26f6de3
Anie162 <nhii264705@gmail.com>
2025-12-09 21:40:01 +0700

    Test web 38

--- reporting-service/frontend.py
+++ reporting-service/frontend.py
@@ -307,14 +307,7 @@ def view_manager(headers):
                     with c1: 
                         if r.get('MediaURL'): st.image(r['MediaURL'], caption="Hiện trường")
                     with c2:
-                        u_res = api_request("GET", f"{GENERAL_SERVICE_URL}/api/users", headers=headers)
-                        if u_res and u_res.status_code == 200:
-                            all_users = u_res.json()
-                            # Tìm user có ID trùng với ReporterID
-                            user_obj = next((u for u in all_users if u.get("UserID") == r.get('ReporterId') or u.get("_id") == r.get('ReporterId')), None)
-                            if user_obj:
-                                reporter_name = user_obj.get("Name") or user_obj.get("name") or "No Name"
-                                st.write(f"**Người báo:** `{reporter_name}`")
+                        st.write(f"**Người báo:** `{r.get('ReporterID', 'N/A')}`")
                         st.write(f"**Mô tả:** {r.get('Content')}")
                         st.write("---")
                         st.write("#### 🛠️ Giao Việc")
