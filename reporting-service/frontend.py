fe822a91981ced36e78501c0452d6a38a0bf190a
Anie162 <nhii264705@gmail.com>
2025-12-09 20:10:45 +0700

    Test web 35

--- reporting-service/frontend.py
+++ reporting-service/frontend.py
@@ -381,7 +381,6 @@ def view_technician(headers):
                 if not new_tasks: st.write("Không có nhiệm vụ mới.")
                 for task in new_tasks:
                     # Lấy ID an toàn (ưu tiên _id, id, TaskId...)
-                    st.write(task)
                     tid = str(task.get('_id') or task.get('id') or task.get('TaskId') or task.get('taskCode') or '')
                     
                     report_id = task.get("reportId") or task.get("ReportId")
@@ -431,7 +430,7 @@ def view_technician(headers):
             with tab2:
                 if not active_tasks: st.write("Chưa có nhiệm vụ đang làm.")
                 for task in active_tasks:
-                    tid = task.get('id') or task.get('TaskId')
+                    tid = str(task.get('taskCode') or task.get('taskId') or task.get('_id') or '')
                     status = task.get('status')
                     report_id = task.get("reportId")
                     
